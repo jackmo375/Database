15a56165ed3de1ca90b8dd3c2a073a089928c298
Jack Morrice <jack@jack>
2020-02-28 16:32:39 +0200

    run.sh file updated

---
+++ config.sh
@@ -0,0 +1,9 @@
+# enviroment shell variables
+## change to ones appropriate for your machine
+database=/home/jack/Local/SickleAfrica/Database/
+
+dat=${database}data/
+src=${database}src/
+tst=${database}test/
+rep=${database}reports/
+
--- run.sh
+++ run.sh
@@ -1,1 +1,29 @@
+#
+#	SICKLE IN AFRICA
+#	automatic data transfer protocol
+#
+#######################################
+#######################################
+
 #!/bin/bash
+
+source ./config.sh
+
+#
+#	Pipeline
+#
+
+# 1. send data request to REDCap
+
+# 2. download raw data
+
+# 3. clean data
+python ${src}3_cleanData.py \
+	--tempDir ${dat} \
+	--repDir ${rep}
+
+# 4. convert clean data to SQL format
+
+# 5. send clean data to SIA database
+
+# 6. distribute quality reports
